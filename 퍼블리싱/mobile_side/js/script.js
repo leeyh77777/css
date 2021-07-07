c088d929db6c1c9935f5f76077393cd5a93bb91f
leeyh77777 <lyh071111@gmail.com>
2021-07-07 18:06:49 +0900

    코지샵 퍼블리싱 + css 학습

---
+++ 퍼블리싱/mobile_side/js/script.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    document.querySelector(".show_menu").onclick = function() {
+        const sideMenu = document.querySelector(".side_menu")
+        sideMenu.classList.add("on");
+    };
+
+
+// 메뉴닫기
+    document.querySelector(".close_menu").onclick = function() {
+        const sideMenu = document.querySelector(".side_menu")
+        sideMenu.classList.remove("on");
+    };
+};
